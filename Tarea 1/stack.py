540aa59255bbe73d3914bd4eb5161de193910bbe
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2025-03-29 14:52:52 -0600

    feat add stack

---
+++ Tarea 1/stack.py
@@ -0,0 +1,9 @@
+class Stack:
+    """
+    Esta clase busca implementar la estructura Last In First Out que es una pila o stack
+    """
+    # Inilizamos la pila vacia, ya que no hay nada.
+    def __init__(self):
+        self.items = []
+        
+    
