b403939ad6b47eb29117f8e262a603d795f7eed9
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2025-03-29 15:04:48 -0600

    feat add stack

--- Tarea 1/stack.py
+++ Tarea 1/stack.py
@@ -5,5 +5,35 @@ class Stack:
     # Inilizamos la pila vacia, ya que no hay nada.
     def __init__(self):
         self.items = []
-        
+
+    #Añadimos un elemento de  arriba de la pila
+    def push(self,item):
+        self.items.append(item)
     
+    # Aqui eliminamos un elemento de arriba y lanzamos un error si la pila ya esta vacia
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("La pila esta vacia")
+        return self.items.pop()
+    
+    #Regresa el elemento de hasta arriba y da un error si esta vacio
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("La pila esta vacia")
+        return self.items[-1]
+    
+    # Checa si la pila esta vacia
+    def is_empty(self):
+        return len(self.items) == 0
+
+    # Regresa el numero de elementos en la pila
+    def size(self):
+        return len(self.items)
+
+    # Limpiamos todos los elementos en la pila
+    def clear(self):
+        self.items = []
+
+    # Refresa la pila en formato de texto
+    def __str__(self):
+        return str(self.items)
